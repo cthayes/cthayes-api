de992ecbfcc868478d8c66e8e7c5080594c0ed94
Christopher Hayes <chris@cthayes.net>
2017-09-06 17:34:07 -0400

    Add cors and log when accessed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const cors = require('koa-cors');
 
 const app = new Koa();
 const router = new Router();
@@ -13,15 +14,18 @@ if (process.argv.length == 3) {
 	port = process.argv[2]
 }
 
+app.use(cors());
 
 router.get('/education', async function (ctx, next) {
-  ctx.body = JSON.stringify(education)
-  ctx.response.status = 200;
+	console.log('Responding on /education')
+	ctx.body = JSON.stringify(education)
+	ctx.response.status = 200;
 });
 
 router.get('/experiences', async function (ctx, next) {
-  ctx.body = JSON.stringify(experiences)
-  ctx.response.status = 200;
+	console.log('Responding on /experiences')
+	ctx.body = JSON.stringify(experiences)
+	ctx.response.status = 200;
 });
 
 app.use(router.routes());
